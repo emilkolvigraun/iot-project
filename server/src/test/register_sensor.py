0397cafe57b1d05be3a95cc20ce9aa320481ea9b
mortenabra <sezure@gmail.com>
2020-05-26 10:40:35 +0200

    small fix
    Revert ip

--- server/src/test/register_sensor.py
+++ server/src/test/register_sensor.py
@@ -45,7 +45,7 @@ client = MQTTClient(receiver)
 client.start_loop()
 
 for i in range(0, 150):
-    client.publish('sensor/alert', 'test')
+    client.publish('sensor/alert', '5')
 
 # for a in ['e2b81c5a-1188-48d5-a87e-2d261bf7e71f', '71e19761-ebde-4ff9-9a71-3769ee221a9a', '13b4e90e-7ea7-46e4-92a4-669779139777']:
 #     client.publish("archiver/stop", "LIVING_ROOM_30:AE:A4:DC:9A:38,"+a)
